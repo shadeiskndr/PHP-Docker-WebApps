652d0ed51db03d414613c8d05a551ccf255d83fd
Shahathir Iskandar <shahathiriskandar42@gmail.com>
2025-01-19 23:56:59 +0800

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'selector',
-  // content: ["./*.{html,js,php}", "./**/*.{html,js,php}"],
   content: ["./*.php", "./**/*.php", "./**/**/*.php", "./includes/*.php", "./calculator/*.php"],
    theme: {
      extend: {},
